965fb075808a89b5069376642cb2dd699588eb1b
DonGranda <lesliekofiameg@gmail.com>
2025-06-15 19:53:27 +0000

    servername was commented out

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,7 +4,7 @@ sudo yum -y install httpd
 sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
 <VirtualHost *:80>
   ServerAdmin root@localhost
-  ServerName app.nextwork.com
+  # ServerName app.nextwork.com
   DefaultType text/html
   ProxyRequests off
   ProxyPreserveHost On
